9ad35052aec2e3ca0a89a3916f2611d6babcd444
FelipeMDantas <felipemdantas@terra.com.br>
2024-05-18 23:50:54 -0300

    Accounts-related hooks and components created.

---
+++ features/accounts/components/account-form.tsx
@@ -0,0 +1,70 @@
+import { insertAccountSchema } from "@/db/schema";
+import { Form, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = insertAccountSchema.pick({ name: true });
+
+type FormValues = z.input<typeof formSchema>;
+
+type Props = {
+  id?: string;
+  defaultValues?: FormValues;
+  onSubmit: (values: FormValues) => void;
+  onDelete?: () => void;
+  disabled?: boolean;
+};
+
+export const AccountForm = ({
+  id,
+  defaultValues,
+  onSubmit,
+  onDelete,
+  disabled,
+}: Props) => {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: defaultValues,
+  });
+
+  const handleSubmit = (values: FormValues) => {
+    console.log({ values });
+  };
+
+  const handleDelete = (values: FormValues) => {
+    onDelete?.();
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="space-y-4 pt-4"
+      >
+        <FormField
+          name="name"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input
+                  disabled={disabled}
+                  placeholder="e.g.: Cash, Bank, Credit Card"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
